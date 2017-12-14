fd8b953536654eacedc0205fef6ee383360516b0
Niklas Carlsson <niklas.carlsson1994@gmail.com>
2017-12-14 20:21:50 +0100

    hej

--- ha5/b3.py
+++ ha5/b3.py
@@ -21,7 +21,7 @@ def long_def_form(num_hex):
 def der(num):
 	if num < 0:
 		return "num < 0 not supported"
-	
+
 	num_hex = bytes_to_str(int_to_bytes(num)).zfill(2)
 	
 	if len(bin(num)[2:]) % 8 == 0:
@@ -61,7 +61,18 @@ def base64_der_rsa(p, q):
 	e2 = d % (q - 1)
 	coeff = calc_coeff(q, p)
 	
-	return "303c" + der(0) + der(n) + der(e) + der(d) + der(p) + der(q) + der(e1) + der(e2) + der(coeff)
+	total_key = der(0) + der(n) + der(e) + der(d) + der(p) + der(q) + der(e1) + der(e2) + der(coeff)
+	length = bytes_to_str(int_to_bytes(len(total_key) // 2))
+
+	print(length)
+
+	if len(length) > 2:
+		fill_to = (len(length) // 2)
+		long_def = 128 + (len(length) // 2)
+		length = bytes_to_str(int_to_bytes(long_def)).zfill(2) + length.zfill(fill_to)
+		
+	print(length)
+	return "30" + length + total_key
 
 def main():
 	e = 2530368937
@@ -70,14 +81,16 @@ def main():
 	#print(short_def_form(e))
 	#print(short_def_form(l))
 	
-	print(der(l))
+	#der(123)
+	#print(der(l))
 	
 	p = 139721121696950524826588106850589277149201407609721772094240512732263435522747938311240453050931930261483801083660740974606647762343797901776568952627044034430252415109426271529273025919247232149498325412099418785867055970264559033471714066901728022294156913563009971882292507967574638004022912842160046962763
 	q = 141482624370070397331659016840167171669762175617573550670131965177212458081250216130985545188965601581445995499595853199665045326236858265192627970970480636850683227427420000655754305398076045013588894161738893242561531526805416653594689480170103763171879023351810966896841177322118521251310975456956247827719
 	
-	key = base64_der_rsa(2530368937, 2612592767)
+	key = base64_der_rsa(p, q)
 	key_64 = base64.b64encode(binascii.unhexlify(key))
 	print("rsa:", key)
+	print()
 	print("base64:", str(key_64)[2:-1])
 	
 if __name__ == "__main__":
